12b147196da25587a3bfba79a06553a757b5d3d7
travimesa <travis@avimesa.com>
2019-01-08 10:25:27 -0800

    - updated license - prepping for npm publish

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * COPYRIGHT: This file and the source codes contained herein ("document") are
+ * the property of Avimesa, Inc.  Copyright 2016-2019, Avimesa, Inc.
+ *
+ * LICENSE:  Avimesa, Inc. grants the RECIPIENT a worldwide, royalty free,
+ * limited license to use the source codes in this document as specified
+ * in the Avimesa Open License:  http://avimesa.com/openlicense.txt
+ */
+
+console.log("Please update the .env file");
+console.log("To run an example, follow this format in the terminal:");
+console.log("  node accounting-records/index.js");
